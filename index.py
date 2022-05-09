397acb3038e71e88508192ae0fa243618c9a83a9
AlexFoxRus <58283264+AlexFoxRus@users.noreply.github.com>
2022-05-09 19:04:17 +0300

    оптимизировать

---
+++ index.py
@@ -0,0 +1,57 @@
+#if a2 > a1 and b2 > b1 and a2 > b1:
+#    print(x)
+#elif a2 > a1 and b2 > b1 and a2 < b1:
+#    print(a2, b1)
+#elif a2 > a1 and b2 > b1 and a2 == b1:
+#    print(a2)
+#elif a2 == a1 and b2 > b1 and b2 > a2:
+#    print(a2, b1)
+#elif a2 > a1 and b2 == b1:
+#    print(a2, b1)
+#
+#elif a2 < a1 and b2 < b1 and b2 < a1:
+#    print(x)
+#elif a2 < a1 and b2 < b1 and b2 > a1:
+#    print(a1, b2)
+#elif a2 < a1 and b2 < b1 and b2 ==a1:
+#    print(a1)
+#elif a2 < a1 and b2 == b1 and b2 > a1:
+#    print(a1, b2)
+#elif a2 == a1 and b2 < b1:
+#    print(a1, b2)
+#
+#elif a2 > a1 and b2 > a1 and b2 < b1 and a2 < b1:
+#    print(a2, b2)
+#elif a2 < a1 and b2 > a1 and b2 > b1 and a2 < b1:
+#    print(a1, b1)
+#elif a2 == a1 and b2 == b1:
+#    print(a1, b1)
+
+if a2 > a1 and b2 > b1 and a2 > b1:
+    print(x)
+elif a2 > a1 and b2 > b1 and a2 < b1:
+    print(a2, b1)
+elif a2 > a1 and b2 > b1 and a2 == b1:
+    print(a2)
+elif a2 == a1 and b2 > b1 and b2 > a2:
+    print(a2, b1)
+elif a2 > a1 and b2 == b1:
+    print(a2, b1)
+
+elif a2 < a1 and b2 < b1 and b2 < a1:
+    print(x)
+elif a2 < a1 and b2 < b1 and b2 > a1:
+    print(a1, b2)
+elif a2 < a1 and b2 < b1 and b2 ==a1:
+    print(a1)
+elif a2 < a1 and b2 == b1 and b2 > a1:
+    print(a1, b2)
+elif a2 == a1 and b2 < b1:
+    print(a1, b2)
+
+elif a2 > a1 and b2 > a1 and b2 < b1 and a2 < b1:
+    print(a2, b2)
+elif a2 < a1 and b2 > a1 and b2 > b1 and a2 < b1:
+    print(a1, b1)
+elif a2 == a1 and b2 == b1:
+    print(a1, b1)
