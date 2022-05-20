171306fc42d3457443c48ba4e27066c4eb3de9f7
AlexFoxRus <58283264+AlexFoxRus@users.noreply.github.com>
2022-05-20 16:38:20 +0300

    1

--- index.py
+++ index.py
@@ -1,57 +1,20 @@
-#if a2 > a1 and b2 > b1 and a2 > b1:
-#    print(x)
-#elif a2 > a1 and b2 > b1 and a2 < b1:
-#    print(a2, b1)
-#elif a2 > a1 and b2 > b1 and a2 == b1:
-#    print(a2)
-#elif a2 == a1 and b2 > b1 and b2 > a2:
-#    print(a2, b1)
-#elif a2 > a1 and b2 == b1:
-#    print(a2, b1)
-#
-#elif a2 < a1 and b2 < b1 and b2 < a1:
-#    print(x)
-#elif a2 < a1 and b2 < b1 and b2 > a1:
-#    print(a1, b2)
-#elif a2 < a1 and b2 < b1 and b2 ==a1:
-#    print(a1)
-#elif a2 < a1 and b2 == b1 and b2 > a1:
-#    print(a1, b2)
-#elif a2 == a1 and b2 < b1:
-#    print(a1, b2)
-#
-#elif a2 > a1 and b2 > a1 and b2 < b1 and a2 < b1:
-#    print(a2, b2)
-#elif a2 < a1 and b2 > a1 and b2 > b1 and a2 < b1:
-#    print(a1, b1)
-#elif a2 == a1 and b2 == b1:
-#    print(a1, b1)
+a = int(input())
+b_max = a % 10
+b_min = a % 10
+while a != 0:
+    b = a % 10
+    if b_min < b >= b_max:
+        b_max = b
+        b_min = b_min
+    elif b_min > b < b_max:
+        b_min = b
+        b_max = b_max      
+    a = a // 10
+print("Максимальная цифра равна ", b_max)
+print("Минимальная цифра равна ", b_min)
 
-if a2 > a1 and b2 > b1 and a2 > b1:
-    print(x)
-elif a2 > a1 and b2 > b1 and a2 < b1:
-    print(a2, b1)
-elif a2 > a1 and b2 > b1 and a2 == b1:
-    print(a2)
-elif a2 == a1 and b2 > b1 and b2 > a2:
-    print(a2, b1)
-elif a2 > a1 and b2 == b1:
-    print(a2, b1)
-
-elif a2 < a1 and b2 < b1 and b2 < a1:
-    print(x)
-elif a2 < a1 and b2 < b1 and b2 > a1:
-    print(a1, b2)
-elif a2 < a1 and b2 < b1 and b2 ==a1:
-    print(a1)
-elif a2 < a1 and b2 == b1 and b2 > a1:
-    print(a1, b2)
-elif a2 == a1 and b2 < b1:
-    print(a1, b2)
-
-elif a2 > a1 and b2 > a1 and b2 < b1 and a2 < b1:
-    print(a2, b2)
-elif a2 < a1 and b2 > a1 and b2 > b1 and a2 < b1:
-    print(a1, b1)
-elif a2 == a1 and b2 == b1:
-    print(a1, b1)
+#альтернативное решение:
+#a = int(input())
+#a = str(a)
+#print(max(a))
+#print(min(a))
